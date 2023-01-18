19f8d08cb531b6fb9f90fb0be0976f8899234c3b
Semih Erguven <serguven@my.bcit.ca>
2023-01-18 12:15:05 -0800

    updated url

--- Frontend/script.js
+++ Frontend/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		const message = $('#chatbot-input').val();
 		$.ajax({
 			type: 'POST',
-			url: 'http://localhost:3000/chatbot',
+            url: 'https://funfactsaboutnumbers.onrender.com/chatbot',
 			data: {
 				message: message
 			},
